7614d7240ba207a6fc47ea6e5145684f6022a5cd
sanjib001 <sanjibpandey94@gmail.com>
2026-02-12 18:51:45 +0545

    Adds basic concept of Next js(routing)

---
+++ my-next-app/src/app/contact/[id]/page.js
@@ -0,0 +1,4 @@
+
+export default function DynamicPage() {
+  return ( <h1>This is DynamicPage</h1>);
+}
---
+++ my-next-app/src/app/contact/inner/page.js
@@ -0,0 +1,4 @@
+
+export default function Inner() {
+  return ( <h1>This is InnerPage</h1>);
+}
