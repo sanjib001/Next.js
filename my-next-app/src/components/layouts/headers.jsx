43d07f2d0df30c7d612fd5be77ad71006f08d6fb
sanjib001 <sanjibpandey94@gmail.com>
2026-04-28 16:27:54 +0545

    Padding in hearders

--- my-next-app/src/components/layouts/headers.jsx
+++ my-next-app/src/components/layouts/headers.jsx
@@ -2,7 +2,7 @@ import DesktopNav from "./desktop-nav.jsx";
 
 const Headers = () => {
     return (
-        <header className="p-4 md:p-6 lg:p-12 bg-transparent backdrop-blur-xl border-1 border-b-gary-600 sticky top-0 shawdow-md z-1000">
+        <header className="px-4 py-2 md:px-6 lg:px-12 bg-transparent backdrop-blur-xl border-1 border-b-gary-600 sticky top-0 shawdow-md z-1000">
             <DesktopNav />
         </header> 
     )
