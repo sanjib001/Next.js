f92c08fce3753f88ed9fefe1aaaeda97ecfd4cac
sanjib001 <sanjibpandey94@gmail.com>
2026-04-28 16:23:00 +0545

    padding fix in header

--- my-next-app/src/components/layouts/headers.jsx
+++ my-next-app/src/components/layouts/headers.jsx
@@ -2,7 +2,7 @@ import DesktopNav from "./desktop-nav.jsx";
 
 const Headers = () => {
     return (
-        <header className="px-10 py-3 bg-transparent backdrop-blur-xl border-1 border-b-gary-600 sticky top-0 shawdow-md z-1000">
+        <header className="p-4 md:p-6 lg:p-12 bg-transparent backdrop-blur-xl border-1 border-b-gary-600 sticky top-0 shawdow-md z-1000">
             <DesktopNav />
         </header> 
     )
